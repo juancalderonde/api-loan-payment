c56e55213c57fc12d03a3025ad96f2425e37b8b1
esteban-calderon5 <esteban.calderon5@gmail.coom>
2023-06-08 22:50:12 -0500

    improve in return of payment table

--- loan-payment/src/main/java/com/juancalderondev/loanpayment/Controllers/LoanController.java
+++ loan-payment/src/main/java/com/juancalderondev/loanpayment/Controllers/LoanController.java
@@ -14,32 +14,35 @@ public class LoanController {
 	@Autowired
 	private LoanService loanService;
 	
+	/**
+	 * creates the table with the payment in every of the numberOfPayments
+	 * @param periodRate interest rate for each payment period
+	 * @param numberOfPayments total number of payments
+	 * @param amount total amount of the loan
+	 * @return
+	 */
 	@GetMapping("/api/loanpayment/create")
 	public String createLoanPaymentTable(@RequestParam("periodRate") String periodRate,
 										 @RequestParam("numberOfPayments") String numberOfPayments,
 										 @RequestParam("amount") String amount) {
+		//as the values are received as string, this transform to numeric
 		final Double convertedPeriodRate = Double.parseDouble(periodRate);
 		final Integer convertedNumOfPayments = Integer.parseInt(numberOfPayments);
 		final Double convertedAmount = Double.parseDouble(amount);
 		
+		//new instance of the loan specs and creation of payment table
 		Loan createLoan = new Loan(convertedPeriodRate,convertedNumOfPayments,convertedAmount);
 		createLoan.setPaymentTable(loanService.createPaymentTable(createLoan));
 		
-		
-		
 		try {
-			// Create an instance of ObjectMapper (Jackson)
+			// Create an instance of ObjectMapper use to transform list to json object
 			ObjectMapper objectMapper = new ObjectMapper();
-	
-			// Convert the list to JSON
 			String jsonString = objectMapper.writeValueAsString(createLoan.getPaymentTable());
+	
 			return jsonString;
 		}catch(Exception e) {
 			 e.printStackTrace();
 		}
-		// Set the JSON string as the response body
-		//response.setContentType("application/json");
-		//response.getWriter().write(jsonString);
 		return null;
 	}
 	
--- loan-payment/src/main/java/com/juancalderondev/loanpayment/Services/LoanService.java
+++ loan-payment/src/main/java/com/juancalderondev/loanpayment/Services/LoanService.java
@@ -14,8 +14,15 @@ import com.juancalderondev.loanpayment.Models.Loan;
 @Service
 public class LoanService {
 	
+	/**
+	 * this method creates a nested list containing the id of the payment, 
+	 * the interest to be paid, the principal, the total payment and the remaining outstanding
+	 * @param loanSpecs is the object containing the specs for the loan 
+	 * @return a nested list with the loan payment table
+	 */
 	public List<Map<String, Object>> createPaymentTable(Loan loanSpecs){
 		
+		//variables to be used in the proccess
 		List<Map<String,Object>> paymentTable = new ArrayList<>();
 		Double mainPayment = loanSpecs.getAmount() / annuityCalculation(loanSpecs);
 		int numberOfPayments = loanSpecs.getNumberOfPayments();
@@ -23,11 +30,12 @@ public class LoanService {
 		Double principalPay = 0.0;
 		Double lastOustanding = 0.0;
 		
-		
+		//fills the list object with the result of the payment table
 		for (int i=0; i<=numberOfPayments;i++) {
 			Map<String,Object> rowData = new HashMap<>();
 			if(i == 0) {
-				rowData.put("periodo", 0);
+				//starting point does not have any calculation
+				rowData.put("period", 0);
 				rowData.put("interest", 0);
 				rowData.put("principal", 0);
 				rowData.put("totalPayment", 0);
@@ -37,7 +45,7 @@ public class LoanService {
 				lastOustanding = Double.parseDouble(paymentTable.get(i-1).get("outstanding").toString());
 				interestToPay =  lastOustanding * loanSpecs.getPeriodRate();
 				principalPay = mainPayment - interestToPay;
-				rowData.put("periodo",i);
+				rowData.put("period",i);
 				rowData.put("interest", interestToPay);
 				rowData.put("principal", principalPay);
 				rowData.put("totalPayment", mainPayment);
@@ -50,7 +58,12 @@ public class LoanService {
 		return paymentTable;
 	}
 	
-	
+	/**
+	 * calculates the annuity factor which can be calculated as [1-(1+i)^-n]/i
+	 * this will allow to calculate the payment of the loan
+	 * @param loanSpecs contains the loan specification for calculating the annuity
+	 * @return annuity factor as described above.
+	 */
 	private Double annuityCalculation(Loan loanSpecs) {
 		return (1-(Math.pow(1 + loanSpecs.getPeriodRate(), -loanSpecs.getNumberOfPayments() *1.0)))/loanSpecs.getPeriodRate();
 	}
